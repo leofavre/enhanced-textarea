aa79e22ed483eafcacec75721bb1b4ec59c15c58
Leonardo Favre <leo@leofavre.com>
2020-09-15 17:29:42 -0300

    chore: Implement `autoResize` attribute and property

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 export class AutomaticallyResizableTextArea extends HTMLTextAreaElement {
+  get autoresize () {
+    return this.autoResize;
+  }
 
+  set autoresize (value) {
+    this.autoResize = value;
+  }
+
+  get autoResize () {
+    return this.hasAttribute('autoresize');
+  }
+
+  set autoResize (value) {
+    if (value) {
+      this.setAttribute('autoresize', '');
+    } else {
+      this.removeAttribute('autoresize');
+    }
+  }
 }
