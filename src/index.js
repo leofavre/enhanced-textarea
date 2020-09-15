aa4348e7b387fa46e6f90715f8f4d2e4d8830d33
Leonardo Favre <leo@leofavre.com>
2020-09-15 18:54:31 -0300

    chore: Implement listeners and resize behavior

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 export class AutomaticallyResizableTextArea extends HTMLTextAreaElement {
+  constructor () {
+    super();
+    this.__handleChange = this.__handleChange.bind(this);
+  }
+
   get autoresize () {
     return this.autoResize;
   }
@@ -16,6 +21,44 @@ export class AutomaticallyResizableTextArea extends HTMLTextAreaElement {
       this.setAttribute('autoresize', '');
     } else {
       this.removeAttribute('autoresize');
+    }
+  }
+
+  static get observedAttributes () {
+    return [
+      ...super.observedAttributes || [],
+      'autoresize',
+      'rows'
+    ];
+  }
+
+  attributeChangedCallback (...args) {
+    super.attributeChangedCallback && super.attributeChangedCallback(...args);
+    const [attrName] = args;
+
+    if (attrName === 'autoresize' || attrName === 'rows') {
+      this.__handleChange();
+    }
+  }
+
+  connectedCallback () {
+    super.connectedCallback && super.connectedCallback();
+    this.__resizeObserver = new ResizeObserver(this.__handleChange);
+    this.__resizeObserver.observe(this);
+    this.addEventListener('input', this.__handleChange);
+  }
+
+  disconnectedCallback () {
+    super.disconnectedCallback && super.disconnectedCallback();
+    this.__resizeObserver.unobserve(this);
+    this.removeEventListener('input', this.__handleChange);
+  }
+
+  __handleChange () {
+    if (this.autoResize) {
+      const offset = this.offsetHeight - this.clientHeight;
+      this.style.minHeight = 'auto';
+      this.style.minHeight = this.scrollHeight + offset + 'px';
     }
   }
 }
