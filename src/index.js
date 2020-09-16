3430eb2535150a5028e586ea4be1acd96ba45c78
Leonardo Favre <leo@leofavre.com>
2020-09-16 03:34:04 -0300

    chore: Implement resize logic along with user resize

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const OBSERVED_ATTRS = ['autoheight', 'rows', 'class', 'style'];
+const OBSERVED_ATTRS = ['autoheight', 'rows', 'class'];
 
 export class AutomaticallyResizableTextArea extends HTMLTextAreaElement {
   constructor () {
     super();
     this.__handleChange = this.__handleChange.bind(this);
+    this.__handleUserResize = this.__handleUserResize.bind(this);
   }
 
   get autoheight () {
@@ -34,7 +35,7 @@ export class AutomaticallyResizableTextArea extends HTMLTextAreaElement {
     super.attributeChangedCallback && super.attributeChangedCallback(...args);
     const [attrName] = args;
 
-    if (OBSERVED_ATTRS.includes[attrName]) {
+    if (OBSERVED_ATTRS.includes(attrName)) {
       this.__handleChange();
     }
   }
@@ -44,19 +45,34 @@ export class AutomaticallyResizableTextArea extends HTMLTextAreaElement {
     this.__resizeObserver = new ResizeObserver(this.__handleChange);
     this.__resizeObserver.observe(this);
     this.addEventListener('input', this.__handleChange);
+    this.addEventListener('pointerup', this.__handleUserResize);
+    this.addEventListener('pointerdown', this.__handleUserResize);
   }
 
   disconnectedCallback () {
     super.disconnectedCallback && super.disconnectedCallback();
     this.__resizeObserver.unobserve(this);
     this.removeEventListener('input', this.__handleChange);
+    this.removeEventListener('pointerup', this.__handleUserResize);
+    this.removeEventListener('pointerdown', this.__handleUserResize);
   }
 
   __handleChange () {
+    clearTimeout(this.__resizeTimeout);
+
     if (this.autoHeight) {
       const offset = this.offsetHeight - this.clientHeight;
       this.style.minHeight = 'auto';
-      this.style.minHeight = this.scrollHeight + offset + 'px';
+
+      if (!this.__isUserResizing) {
+        this.style.minHeight = `${Math.max(this.scrollHeight + offset)}px`;
+      } else {
+        this.__resizeTimeout = setTimeout(this.__handleChange, 50);
+      }
     }
+  }
+
+  __handleUserResize ({ type }) {
+    this.__isUserResizing = type === 'pointerdown';
   }
 }
