66ee527332ba1ac77856d09a2cef09ba96e290da
Leonardo Favre <leo@leofavre.com>
2020-09-16 13:19:04 -0300

    chore: Add more native attributes and properties to constants and differentiate between them

--- src/constants.js
+++ src/constants.js
@@ -1,20 +1,48 @@
-export const SHARED_ATTRIBUTES = [
+import arrayToLowerCase from './helpers/arrayToLowerCase.js';
+
+export const NATIVE_REFLECTED_PROPERTIES = [
+  'autofocus',
+  'cols',
+  'dirName',
+  'disabled',
+  'form',
+  'minLength',
+  'maxLength',
+  'placeholder',
+  'readOnly',
+  'required',
   'rows',
-  'cols'
+  'wrap'
 ];
 
-export const PARENT_ATTRIBUTES = [
-  'autoheight',
+export const NATIVE_NON_REFLECTED_PROPERTIES = [
+  'defaultValue',
+  'selectionDirection',
+  'selectionStart',
+  'selectionEnd',
+  'value'
+];
+
+export const CUSTOM_PROPERTIES = [
+  'autoheight'
+];
+
+export const NATIVE_REFLECTED_ATTRIBUTES =
+  arrayToLowerCase(NATIVE_REFLECTED_PROPERTIES);
+
+export const NATIVE_NON_REFLECTED_ATTRIBUTES =
+  arrayToLowerCase(NATIVE_NON_REFLECTED_PROPERTIES);
+
+export const CUSTOM_ATTRIBTUES = arrayToLowerCase(CUSTOM_PROPERTIES);
+
+export const OBSERVED_ATTRIBUTES = [
+  ...CUSTOM_ATTRIBTUES,
+  ...NATIVE_REFLECTED_ATTRIBUTES,
   'class'
 ];
 
-export const OBSERVED_ATTRIBUTES = [
-  ...PARENT_ATTRIBUTES,
-  ...SHARED_ATTRIBUTES
-];
-
 export const LAZY_PROPERTIES = [
-  'autoheight',
-  'value',
-  ...SHARED_ATTRIBUTES
+  ...CUSTOM_PROPERTIES,
+  ...NATIVE_NON_REFLECTED_PROPERTIES,
+  ...NATIVE_REFLECTED_PROPERTIES
 ];
