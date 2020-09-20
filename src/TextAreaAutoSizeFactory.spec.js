97f3f35df41cf9a08706766f22f28ecc9938e374
Leonardo Favre <leo@leofavre.com>
2020-09-20 19:24:12 -0300

    chore: Add more tests to TextAreaAutoSizeFactory

--- src/TextAreaAutoSizeFactory.spec.js
+++ src/TextAreaAutoSizeFactory.spec.js
@@ -1,6 +1,8 @@
 jest.mock('./helpers/getCoercedAttr.js');
+jest.mock('./helpers/setAttr.js');
 
 import getCoercedAttr from './helpers/getCoercedAttr.js';
+import setAttr from './helpers/setAttr.js';
 import TextAreaAutoSizeFactory from './TextAreaAutoSizeFactory.js';
 
 let BaseClass;
@@ -19,11 +21,58 @@ describe('TextAreaAutoSizeFactory', () => {
     expect(ResultClass.prototype instanceof BaseClass).toBe(true);
   });
 
-  describe('get autoheight', () => {
-    test('Laser', () => {
+  describe('.textElement', () => {
+    test('Return the instance itself', () => {
+      expect(element.textElement).toBe(element);
+    });
+  });
+
+  describe('.autoheight', () => {
+    test('Calls getCoercedAttr on get', () => {
       element.autoheight;
       const expectedArgs = [element, 'autoheight', Boolean];
       expect(getCoercedAttr).toHaveBeenCalledWith(...expectedArgs);
+    });
+
+    test('Calls setAttr on set', () => {
+      element.autoheight = true;
+      const expectedArgs = [element, 'autoheight', true];
+      expect(setAttr).toHaveBeenCalledWith(...expectedArgs);
+    });
+  });
+
+  describe('.value', () => {
+    let descriptor;
+
+    beforeEach(() => {
+      BaseClass = class {};
+
+      Object.defineProperty(BaseClass.prototype, 'value', {
+        get: jest.fn(),
+        set: jest.fn()
+      });
+
+      descriptor = Object
+        .getOwnPropertyDescriptor(BaseClass.prototype, 'value');
+
+      Element = TextAreaAutoSizeFactory(BaseClass);
+      element = new Element();
+      element._handleChange = jest.fn();
+    });
+
+    test('Gets super.value', () => {
+      element.value;
+      expect(descriptor.get).toHaveBeenCalled();
+    });
+
+    test('Sets super.value', () => {
+      element.value = 50;
+      expect(descriptor.set).toHaveBeenCalled();
+    });
+
+    test('Calls _handleChange on set', () => {
+      element.value = 50;
+      expect(element._handleChange).toHaveBeenCalled();
     });
   });
 });
