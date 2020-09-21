6edc6abbe9305670bf48a97fbd5e6d4f2c2615fd
Leonardo Favre <leo@leofavre.com>
2020-09-21 20:15:24 -0300

    chore: Standardize test syntax

--- src/helpers/pxToNumber.spec.js
+++ src/helpers/pxToNumber.spec.js
@@ -1,31 +1,31 @@
 import pxToNumber from './pxToNumber.js';
 
 describe('pxToNumber', () => {
-  test('Extracts the numeric value from a measure in pixels', () => {
+  it('Extracts the numeric value from a measure in pixels', () => {
     expect(pxToNumber('20px')).toBe(20);
   });
 
-  test('Handles negative and float values', () => {
+  it('Handles negative and float values', () => {
     expect(pxToNumber('-0.875px')).toBe(-0.875);
   });
 
-  test('Ignores invalid measures', () => {
+  it('Ignores invalid measures', () => {
     expect(pxToNumber('0px20')).toBeUndefined;
   });
 
-  test('Ignores invalid measures that end with px', () => {
+  it('Ignores invalid measures that end with px', () => {
     expect(pxToNumber('-0.89-45.389px')).toBeUndefined;
   });
 
-  test('Ignores entries that are not measures', () => {
+  it('Ignores entries that are not measures', () => {
     expect(pxToNumber('auto')).toBeUndefined;
   });
 
-  test('Ignores empty strings', () => {
+  it('Ignores empty strings', () => {
     expect(pxToNumber('')).toBeUndefined;
   });
 
-  test('Ignores undefined entries', () => {
+  it('Ignores undefined entries', () => {
     expect(pxToNumber()).toBeUndefined;
   });
 });
