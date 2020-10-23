8dde7a0ae8515c3b8329fe3b6ade20ede8c79f78
Leonardo Favre <leo@leofavre.com>
2020-10-23 13:56:08 -0300

    chore: Add more tests

--- src/helpers/getCoercedAttr.spec.ts
+++ src/helpers/getCoercedAttr.spec.ts
@@ -4,8 +4,8 @@ let element;
 describe('setAttr', () => {
   beforeEach(() => {
     element = {
-      hasAttribute: jest.fn(),
-      getAttribute: jest.fn()
+      hasAttribute: jest.fn(() => true),
+      getAttribute: jest.fn(() => '125')
     };
   });
 
@@ -14,13 +14,28 @@ describe('setAttr', () => {
     expect(element.getAttribute).toHaveBeenCalledWith('height');
   });
 
+  it('Returns a string if coerced to a String', () => {
+    const result = getCoercedAttr(element, 'height', String);
+    expect(result).toBe('125');
+  });
+
   it('Calls getAttribute if the attribute is coerced to a Number', () => {
     getCoercedAttr(element, 'readonly', Number);
     expect(element.getAttribute).toHaveBeenCalledWith('readonly');
   });
 
+  it('Returns a number if coerced to a Number', () => {
+    const result = getCoercedAttr(element, 'height', Number);
+    expect(result).toBe(125);
+  });
+
   it('Calls hasAttribute if the attribute is coerced to a Boolean', () => {
     getCoercedAttr(element, 'disabled', Boolean);
     expect(element.hasAttribute).toHaveBeenCalledWith('disabled');
+  });
+
+  it('Returns a boolean if coerced to a Boolean', () => {
+    const result = getCoercedAttr(element, 'height', Boolean);
+    expect(result).toBe(true);
   });
 });
