26d92e010f591505d2e20faf3a110491723b9f8e
Leonardo Favre <leo@leofavre.com>
2020-10-24 17:31:37 -0300

    perf: Use decorator syntax for main export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import WithAutoheight from './with-autoheight/WithAutoheight';
 import WithResizeEvent from './with-resize-event/WithResizeEvent';
 
+@WithAutoheight
+@WithResizeEvent
+class EnhancedTextArea extends HTMLTextAreaElement {}
+
+export default EnhancedTextArea;
+
 export {
   WithAutoheight,
   WithResizeEvent
